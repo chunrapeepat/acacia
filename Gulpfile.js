c490559e6b52794f873c8071ff7c292e0dd12f79
chunza2542 <chunza2542@hotmail.com>
2018-02-01 09:39:58 +0700

    add header comment

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer')
+const headerComment = require('gulp-header-comment')
 
 gulp.task('build-full', function() {
   build({
@@ -19,6 +21,10 @@ function build({ src = '', prefix = '', basename = '' }) {
     .src(src)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(headerComment({
+        file: path.join(__dirname, "header.txt"),
+        encoding: 'utf-8',
+    }))
     .pipe(
       rename({
         basename,
